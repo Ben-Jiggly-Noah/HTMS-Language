42ae8a6780ffd1e740b9b6ca731245fcb60c912e
Jigglypuff JR_Noah <noah.mcmillan3@education.nsw.gov.au>
2024-08-09 13:40:14 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-
+with open("main.htmc", "r") as f:
+    buffer = "";
+    stops = [" ", "\t", "\n"];
+    for ch in f.read():
+        if ch in stops: print("'"+buffer+"'"); buffer="";
+        else: buffer+=ch;
